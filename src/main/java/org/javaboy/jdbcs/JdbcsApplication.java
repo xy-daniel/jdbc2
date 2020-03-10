d2a88ae24637461cdccaa7880b97f9d8d63aa733
86178 <1207350458@qq.com>
2020-03-10 16:20:32 +0800

    初始化

---
+++ src/main/java/org/javaboy/jdbcs/JdbcsApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.jdbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaboy/jdbcs/JdbcsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.jdbcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
