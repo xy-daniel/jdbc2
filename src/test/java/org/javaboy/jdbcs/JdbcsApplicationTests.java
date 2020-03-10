59697a1fd3aea1cd824762f072e29af967fe463a
86178 <1207350458@qq.com>
2020-03-10 16:57:16 +0800

    整合完成

--- src/test/java/org/javaboy/jdbcs/JdbcsApplicationTests.java
+++ src/test/java/org/javaboy/jdbcs/JdbcsApplicationTests.java
@@ -1,13 +1,33 @@
 package org.javaboy.jdbcs;
 
+import org.javaboy.jdbcs.bean.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class JdbcsApplicationTests {
 
+    @Autowired
+    @Qualifier("jdbcTemplateOne")
+    JdbcTemplate jdbcTemplateOne;
+
+    @Resource(name = "jdbcTemplateTwo")
+    JdbcTemplate jdbcTemplateTwo;
+
     @Test
-    void contextLoads() {
+    void jdbcTemplateOne() {
+        System.out.println(jdbcTemplateOne.query("select * from user", new BeanPropertyRowMapper<>(User.class)));
+    }
+
+    @Test
+    void jdbcTemplateTwo() {
+        System.out.println(jdbcTemplateTwo.query("select * from user", new BeanPropertyRowMapper<>(User.class)));
     }
 
 }
